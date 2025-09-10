1a8b79b9bc9ca063f6729d01b7b085caf7349e6f
antoinethirion <antoine.thirion@wavestone.com>
2025-09-10 09:20:31 +0200

    style: refactor code in functions

--- excel.py
+++ excel.py
@@ -13,7 +13,31 @@ regions_data = {
 }
 
 
-def update_capitals_in_excel(filename, capitals_data=None, regions_data=None):
+def get_column_indexes(sheet):
+  headers = [cell.value for cell in sheet[1]]
+  try:
+    country_col_idx = headers.index("Country") + 1
+    capital_col_idx = headers.index("Capital city") + 1
+    region_col_idx = headers.index("Region") + 1
+    return country_col_idx, capital_col_idx, region_col_idx
+  except ValueError as e:
+    print(f"Error: Missing column in Excel file - {e}")
+    return
+
+
+def update_columns(sheet, country_col_idx, columns_to_update):
+  for row in range(2, sheet.max_row + 1):
+    country_cell = sheet.cell(row=row, column=country_col_idx)
+    country_name = country_cell.value
+
+    if country_name in capitals_data:
+      capital_cell = sheet.cell(row=row, column=columns_to_update[0])
+      capital_cell.value = capitals_data[country_name]
+      region_cell = sheet.cell(row=row, column=columns_to_update[1])
+      region_cell.value = regions_data[country_name]
+
+
+def fill_excel(filename, capitals_data=None, regions_data=None):
   """
   Updates the capital cities in an Excel file based on a dictionary of countries and capitals.
   """
@@ -27,24 +51,8 @@ def update_capitals_in_excel(filename, capitals_data=None, regions_data=None):
     workbook = load_workbook(filename)
     sheet = workbook.active
 
-    headers = [cell.value for cell in sheet[1]]
-    try:
-      country_col_idx = headers.index("Country") + 1
-      capital_col_idx = headers.index("Capital city") + 1
-      region_col_idx = headers.index("Region") + 1
-    except ValueError as e:
-      print(f"Error: Missing column in Excel file - {e}")
-      return
-
-    for row in range(2, sheet.max_row + 1):
-      country_cell = sheet.cell(row=row, column=country_col_idx)
-      country_name = country_cell.value
-
-      if country_name in capitals_data:
-        capital_cell = sheet.cell(row=row, column=capital_col_idx)
-        capital_cell.value = capitals_data[country_name]
-        region_cell = sheet.cell(row=row, column=region_col_idx)
-        region_cell.value = regions_data[country_name]
+    country_col_idx, *columns_to_update = get_column_indexes(sheet)
+    update_columns(sheet, country_col_idx, columns_to_update)
 
     workbook.save(filename)
     print(f"Successfully saved changes to {filename}")
@@ -56,4 +64,4 @@ def update_capitals_in_excel(filename, capitals_data=None, regions_data=None):
 
 
 if __name__ == "__main__":
-  update_capitals_in_excel("countries_info.xlsx", capitals_data, regions_data)
+  fill_excel("countries_info.xlsx", capitals_data, regions_data)
